e38461a219fbc2ec8a98aa2b9ca4e88b9f352be6
maxandbe1 <maxandbe1@proton.me>
2026-05-27 21:18:29 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Identity from "./pages/Identity.jsx";
+import Patterns from "./pages/Patterns.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Portal v1</h1>
+      <Identity />
+      <Patterns />
+    </div>
+  );
+}
