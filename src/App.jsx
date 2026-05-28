866f8a8c1e55151909cb59206a4be526d035b052
maxandbe1 <maxandbe1@proton.me>
2026-05-27 21:29:19 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Identity from "./pages/Identity.jsx";
 import Patterns from "./pages/Patterns.jsx";
+import bee from "/bee.png";
 
 export default function App() {
   return (
     <div>
-      <h1>Portal v1</h1>
+      <h1>
+        <img src={bee} alt="Bee" style={{ width: "40px", marginRight: "10px" }} />
+        Bee Portal 🐝
+      </h1>
+
       <Identity />
       <Patterns />
     </div>
