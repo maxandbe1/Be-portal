88e7d107ce943862ae5571b2bc47c7e307468a58
maxandbe1 <maxandbe1@proton.me>
2026-05-27 21:37:28 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import Identity from "./pages/Identity.jsx";
 import Patterns from "./pages/Patterns.jsx";
 import bee from "/bee.png";
+import BeeBanner from "./BeeBanner.jsx";
+import BeeBanner from "./BeeBanner.jsx";
+import BeeBanner from "./BeeBanner.jsx";
+
+<BeeBanner />
+
+
+
+
 
 export default function App() {
   return (
